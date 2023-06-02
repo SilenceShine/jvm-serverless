332e92b86801811a677a016c9bfccc4529223832
SilenceRainly <1292798418@qq.com>
2023-06-02 17:34:43 +0800

    feat: init

---
+++ serverless-server/src/main/java/io/github/SilenceShine/jvm/serverless/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package io.github.SilenceShine.jvm.serverless.server;
+
+/**
+ * @author SilenceShine
+ * @github <a href="https://github.com/SilenceShine">SilenceShine</a>
+ * @since 1.0
+ */
+public class ServerApplication {
+
+
+
+
+
+}
